cd3899f4547cf1dab719050684ad207d1346a298
Niiijima <qagwu11@gmail.com>
2024-02-24 15:09:42 -0800

    airbnb

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,7 +2,6 @@
 """This module defines a class to manage file storage for hbnb clone"""
 
 from models.base_model import Base
-from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
@@ -18,13 +17,8 @@ classes = {'User': User, 'Place': Place, 'Review': Review,
            'State': State, 'City': City}
 
 
-class DBStorage():
-    """new engine, sqlalchemy"""
-    __engine = None
-    __session = None
-
+class DBStorage:
     def __init__(self):
-        """constructor"""
         self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}"
                                       .format(getenv('HBNB_MYSQL_USER'),
                                               getenv('HBNB_MYSQL_PWD'),
@@ -36,35 +30,31 @@ class DBStorage():
             Base.metadata.drop_all(bind=self.__engine, checkfirst=True)
 
     def all(self, cls=None):
-        """queries on the current database session"""
         objects = {}
-        for current_class in classes.values():
-            if current_class is cls or cls is None:
-                for elem in self.__session.query(current_class).all():
-                    objects[elem.__class__.__name__+'.'+elem.id] = elem
+        for cls_name, cls in classes.items():
+            if cls is None or cls_name == cls.__name__:
+                query = self.__session.query(cls)
+                for obj in query.all():
+                    key = "{}.{}".format(obj.__class__.__name__, obj.id)
+                    objects[key] = obj
         return objects
 
     def new(self, obj):
-        """add the object to the current database session"""
         self.__session.add(obj)
 
     def save(self):
-        """ commit all changes of the current database session """
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete from the current database session obj if not None"""
         if obj:
             self.__session.delete(obj)
             self.save()
 
     def reload(self):
-        """ no lo se rik """
         Base.metadata.create_all(self.__engine)
-        session_fac = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(session_fac)
-        self.__session = Session
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
 
     def close(self):
-        """closes the current SQLAlchemy session"""
         self.__session.close()
