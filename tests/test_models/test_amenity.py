5f82db648c8cac04392484aea1b4766274024e3f
Niiijima <qagwu11@gmail.com>
2024-02-24 10:15:02 -0800

    airbnb

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+
+from tests.test_models.test_base_model import test_base_model
 from models.amenity import Amenity
 
-
-class test_Amenity(test_basemodel):
-    """ """
-
+class TestAmenity(test_base_model):
+    
     def __init__(self, *args, **kwargs):
-        """ """
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
-
+    
     def test_name2(self):
-        """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
