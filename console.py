d50e2ab85c861105bc297288bd8b83e4c35fa73b
Niiijima <qagwu11@gmail.com>
2024-02-24 09:57:13 -0800

    airbnb

--- console.py
+++ console.py
@@ -2,16 +2,14 @@
 """ Console Module """
 import cmd
 import sys
-import re
 from models.base_model import BaseModel
-from models.__init__ import storage
+from models.__init import storage
 from models.user import User
 from models.place import Place
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
-
 
 class HBNBCommand(cmd.Cmd):
     """ Contains the functionality for the HBNB console"""
